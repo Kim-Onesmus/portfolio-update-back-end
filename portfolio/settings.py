ab7100149c90587bf123c2b24ec28b704691c83f
Muimi <kimones1122@gmail.com>
2023-06-08 13:49:24 +0300

    Updates

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -78,16 +78,16 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-'default': dj_database_url.config(default='postgresql://postgres:MPyU2z7XcelNPCxUCEPM@containers-us-west-62.railway.app:7952/railway', conn_max_age=1000)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+# 'default': dj_database_url.config(default='postgresql://postgres:MPyU2z7XcelNPCxUCEPM@containers-us-west-62.railway.app:7952/railway', conn_max_age=1000)
+# }
 
 
 # Password validation
