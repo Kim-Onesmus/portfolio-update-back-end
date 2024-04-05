c3e8e0f349fa4c81f19e8775dbc04060a564857e
Kim-Onesmus <kimones1122@gmail.com>
2024-04-05 17:35:38 +0300

    setting database

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -78,16 +78,21 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': '',
+#         'USER': '',
+#         'PASSWORD': '',
 #     }
 # }
-
-DATABASES = {
-    
-}
 
 # DATABASES = {
 # 'default': dj_database_url.config(default='postgresql://postgres:MPyU2z7XcelNPCxUCEPM@containers-us-west-62.railway.app:7952/railway', conn_max_age=1000)
