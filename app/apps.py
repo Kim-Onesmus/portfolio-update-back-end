96a4aee5d760b4bc97378e8a2c2351685f6b7204
Onesmus Kimanzi <78600209+Kim-Onesmus@users.noreply.github.com>
2024-11-26 14:24:09 +0300

    Update apps.py

--- app/apps.py
+++ app/apps.py
@@ -7,19 +7,19 @@ class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
 
-    def ready(self):
-        scheduler = BackgroundScheduler()
-        scheduler.add_job(self.ping_render, 'interval', minutes=10)
-        scheduler.start()
+    # def ready(self):
+    #     scheduler = BackgroundScheduler()
+    #     scheduler.add_job(self.ping_render, 'interval', minutes=10)
+    #     scheduler.start()
 
-    @staticmethod
-    def ping_render():
-        url = 'https://onesmus-kimanzi.onrender.com'
-        try:
-            response = requests.get(url)
-            if response.status_code == 200:
-                print('Successfully pinged Render instance')
-            else:
-                print(f'Failed to ping Render instance, status code: {response.status_code}')
-        except requests.ConnectionError:
-            print('Error connecting to Render instance')
+    # @staticmethod
+    # def ping_render():
+    #     url = 'https://onesmus-kimanzi.onrender.com'
+    #     try:
+    #         response = requests.get(url)
+    #         if response.status_code == 200:
+    #             print('Successfully pinged Render instance')
+    #         else:
+    #             print(f'Failed to ping Render instance, status code: {response.status_code}')
+    #     except requests.ConnectionError:
+    #         print('Error connecting to Render instance')
